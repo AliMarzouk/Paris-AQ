7a0d887684b0ef81e616310286178ddcdc64dcf8
AliMarzouk <ali.marzouk2@gmail.com>
2024-05-18 21:42:51 +0200

    config backend url

--- dashboard/fetchData.js
+++ dashboard/fetchData.js
@@ -3,7 +3,7 @@ const decodeBase64 = (base64) => {
     return new TextDecoder().decode(Uint8Array.from(binString, (m) => m.codePointAt(0)));
 }
 
-const evtSource = new EventSource("http://127.0.0.1:5000/", {
+const evtSource = new EventSource("https://openaq-data-engineer.onrender.com/", {
     withCredentials: true,
 });
 
